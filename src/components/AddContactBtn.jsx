926c2200bf3a1bab389a97d180fe41047e19205b
srinathj-dev <srinath.j@praja.buzz>
2026-06-10 14:56:46 +0530

    removed unnecessary props

--- src/components/AddContactBtn.jsx
+++ src/components/AddContactBtn.jsx
@@ -1,7 +1,7 @@
 const AddContactBtn = ({ gotoAddContact, icon, addButtonName, fontSize }) => {
   return (
     <button
-      className={`btn-primary ${icon} ${fontSize}  drop-shadow-lg drop-shadow-indigo-500/50 flex items-center justify-center content-center`}
+      className={`btn-primary ${icon} drop-shadow-lg drop-shadow-indigo-500/50 flex items-center justify-center content-center`}
       onClick={() => {
         gotoAddContact();
       }}
